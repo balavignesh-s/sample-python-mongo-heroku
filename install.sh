6bafca86d0c0442c46e6cfc7e3c2261f67032cff
balavignesh-s <balavignesh@shippable.com>
2016-01-26 10:58:46 +0530

    tes

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
+#. $HOME/.rvm/scripts/rvm && rvm use ruby-2.0.0-p598 --default && ruby --version || . /usr/local/rvm/bin/rvm && rvm use ruby-2.0.0-p598 --default && ruby --version
+echo $HOME
 path="/usr/local/rvm/bin/rvm"
-homepath="/home/shippable/.rvm/scripts/rvm"
-if [ -e "/usr/bin/rvm" ]
+homepath="$HOME/.rvm/scripts/rvm"
+if [ -e "$homepath" ]
 then
   echo "one"
-  rvm use ruby-2.0.0-p598 --default
+  . $HOME/.rvm/scripts/rvm && rvm use ruby-2.0.0-p598 --default && ruby --version
 elif [ -e "$path" ]
 then
   echo "two"
-  $path use ruby-2.0.0-p598 --default
-elif [ -e "$homepath" ]
-then
-  echo "tHree"
-  $homepath use ruby-2.0.0-p598 --default
+  . ./$path use ruby-2.0.0-p598 --default && ruby --version
+#elif [ -e "$homepath" ]
+#then
+#  echo "tHree"
+#  $homepath use ruby-2.0.0-p598 --default
+#	/home/shippable/.rvm/scripts/rvm list
 fi
